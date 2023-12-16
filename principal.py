f05ab19fee25cd6ba36648181c720fc19dc05a69
Camila Calcagno <solcisolcito17@gmail.com>
2023-12-16 10:21:02 -0300

    cambios

--- principal.py
+++ principal.py
@@ -15,7 +15,9 @@ def grabarArchivo(listado):
 def altaDeProducto(listado):
     codigo = int(input("codigo:"))
     descripcion = input("descripcion: ")
-    producto = {"codigo": codigo, "descripcion": descripcion}
+    disponible = input("esta disponible? ")
+    categoria = input("elegi una categoria para el producto: ")
+    producto = {"codigo": codigo, "descripcion": descripcion, "disponible": disponible, "categoria": categoria}
     listado.append(producto)
     print(producto)
     grabarArchivo(listado)
@@ -26,25 +28,27 @@ def modificarProducto(listado):
     buscarPorCodigo = int(input("codigo de producto a modificar: "))
     for producto in listado:
         if buscarPorCodigo == producto["codigo"]:
-            nuevaDescricion = input(
-                f"{producto['descripcion']} Ingresa nueva descripcion (ingresa 0 para no modificar):"
+            nuevoCodigo = input(
+                f"{producto['codigo']} ingresa el nuevo codigo (ingresa 0 para no modificar): "
             )
-            if nuevaDescricion != "0":
+            nuevaDescricion = input(
+                f"{producto['descripcion']} Ingresa nueva descripcion (ingresa 0 para no modificar): "
+            )
+            if nuevaDescricion != "0" and nuevaDescricion != "":
                 producto["descripcion"] = nuevaDescricion
-            if buscarPorCodigo != "0":
-                producto["codigo"] = buscarPorCodigo
+
+            if nuevoCodigo != "0" and nuevoCodigo != "":
+                producto["codigo"] = int(nuevoCodigo)
 
             grabarArchivo(listado)
             break
-
-
+        
 def eliminarProducto(listado):
     codigoAbuscar = int(input("codigo del producto a eliminar:"))
     for producto in listado:
         if codigoAbuscar == producto["codigo"]:
-            archivo = open("productos.json")
-            json.dump(listado, archivo, indent=2)
-            archivo.close()
+            listado.remove(producto)
+            grabarArchivo(listado)
             break
 
 
@@ -58,6 +62,8 @@ def leerProductos():
     archivo.close()
     return listado
 
+    
+
 
 # aca empieza el programa
 productos = leerProductos()
@@ -69,11 +75,12 @@ while True:
          3- Eliminar producto
          4- Mostrar todos los productos
          5- mostrar pruebas
+         6- categorizar
          0- Salir
          """
     )
     opcion = input("Opcion: ")
-    #opcion = int(opcion)
+    # opcion = int(opcion)
 
     if opcion == "1":
         altaDeProducto(productos)
@@ -85,5 +92,6 @@ while True:
         mostrarTodos(productos)
     elif opcion == "5":
         print("pruebas")
+    
     elif opcion == "0":
         break
